c60b0747dd719ab60b8c72e82aade461f9bbf356
U-ICIIGPC11\admin <ICIIGPC11+admin@ICIIGPC11.intranet.epfl.ch>
2017-05-05 10:42:51 +0200

    fix compiliation errors

--- Assets/ViveCustomController.cs
+++ Assets/ViveCustomController.cs
@@ -25,9 +25,9 @@ public class ViveCustomController : MonoBehaviour {
     private float lastForwardSpeed = 0f;
 
     //TODO make it change with button pressed
-	Vector3 initRight = Vector3.right.Normalize();
-	Vector3 initUp = Vector3.up.Normalize();
-	Vector3 initForward = Vector3.forward.Normalize();
+	Vector3 initRight = Vector3.right.normalized;
+	Vector3 initUp = Vector3.up.normalized;
+	Vector3 initForward = Vector3.forward.normalized;
 
 	// Use this for initialization
 	void Start () {
@@ -40,19 +40,19 @@ public class ViveCustomController : MonoBehaviour {
         print(GetControllerAngles(controllerRight.transform));
     
 	}
-		
+
     // Return pitch value from -180 to 180 based on set up initial pitch
-	float GetControllerAngles(Transform controllerRot)
+    Vector3 GetControllerAngles(Transform controllerRot)
     {
-		Vector3 rightXZ = Vector3.ProjectOnPlane(controllerRot.right, initUp).Normalize();
-		Vector3 upYX = Vector3.ProjectOnPlane(controllerRot.up, initForward).Normalize();
-		Vector3 forwardZY = Vector3.ProjectOnPlane(controllerRot.forward, initRight).Normalize();
+		Vector3 rightXZ = Vector3.ProjectOnPlane(controllerRot.right, initUp).normalized;
+		Vector3 upYX = Vector3.ProjectOnPlane(controllerRot.up, initForward).normalized;
+		Vector3 forwardZY = Vector3.ProjectOnPlane(controllerRot.forward, initRight).normalized;
 
 		float pitch = Mathf.Sign (controllerRot.forward.y) * Mathf.Acos (Vector3.Dot (forwardZY, initForward)) * Mathf.Rad2Deg;
 		float yaw = Mathf.Sign (controllerRot.right.z) * Mathf.Acos (Vector3.Dot (rightXZ, initRight)) * Mathf.Rad2Deg;
 		float roll = Mathf.Sign (controllerRot.up.x) * Mathf.Acos (Vector3.Dot (upYX, initUp)) * Mathf.Rad2Deg;
 
-		return Vector3(pitch, yaw, roll);
+		return new Vector3(pitch , yaw, roll);
     }
 		
     //return val between [-1,1] use with rigidbody.velocity probably
