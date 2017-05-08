5b16a2883c14e36d4cda5af5e9fa19f979b72d60
U-ICIIGPC11\admin <ICIIGPC11+admin@ICIIGPC11.intranet.epfl.ch>
2017-05-08 16:48:12 +0200

    Implement calibration of player coordinate system

--- Assets/ViveCustomController.cs
+++ Assets/ViveCustomController.cs
@@ -6,10 +6,40 @@ using UnityEngine;
 //TODO Turn this class into  a Singleton
 public class ViveCustomController : MonoBehaviour {
 
-    [SerializeField]
-    GameObject controllerRight;
-    [SerializeField]
-    GameObject controllerLeft;
+	[SerializeField]
+	GameObject controller1;
+	[SerializeField]
+	GameObject controller2;
+
+	SteamVR_TrackedObject trackedControllerLeft {
+		get {
+			int controllerLeftIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Leftmost);
+
+			if (controllerLeftIndex == (int)controller1.GetComponent<SteamVR_TrackedObject> ().index) {
+				return controller1.GetComponent<SteamVR_TrackedObject> ();
+			} else if (controllerLeftIndex == (int)controller2.GetComponent<SteamVR_TrackedObject> ().index) {
+				return controller2.GetComponent<SteamVR_TrackedObject> ();
+			} else {
+				return null;
+			}
+		}
+	}
+	SteamVR_TrackedObject trackedControllerRight {
+		get {
+			int controllerRightIndex = SteamVR_Controller.GetDeviceIndex (SteamVR_Controller.DeviceRelation.Rightmost);
+
+			if (controllerRightIndex == (int)controller1.GetComponent<SteamVR_TrackedObject> ().index) {
+				return controller1.GetComponent<SteamVR_TrackedObject> ();
+			} else if (controllerRightIndex == (int)controller2.GetComponent<SteamVR_TrackedObject> ().index) {
+				return controller2.GetComponent<SteamVR_TrackedObject> ();
+			} else {
+				return null;
+			}
+		}
+	}
+
+	SteamVR_Controller.Device controllerLeft { get { return SteamVR_Controller.Input ((int)trackedControllerLeft.index); }}
+	SteamVR_Controller.Device controllerRight { get { return SteamVR_Controller.Input ((int)trackedControllerRight.index); }}
 
     [SerializeField]
     GameObject cubeTest;
@@ -25,31 +55,38 @@ public class ViveCustomController : MonoBehaviour {
     private float lastForwardSpeed = 0f;
 
     //TODO make it change with button pressed
-	Vector3 initRight = Vector3.right.normalized;
-	Vector3 initUp = Vector3.up.normalized;
-	Vector3 initForward = Vector3.forward.normalized;
+	Vector3 initRight = Vector3.right;
+	Vector3 initUp = Vector3.up;
+	Vector3 initForward = Vector3.forward;
 
 	// Use this for initialization
 	void Start () {
- 
-    }
+	
+	}
    
 	// Update is called once per frame
 	void Update () {
-		
-        print(GetControllerAngles(controllerRight.transform));
+
+		// Reset the initial position of the player if the grip button is clicked
+		if (controllerRight.GetPressDown(SteamVR_Controller.ButtonMask.Grip)) {
+			initRight = trackedControllerRight.transform.right;
+			initUp = trackedControllerRight.transform.up;
+			initForward = trackedControllerRight.transform.forward;
+		}
+
+		print(GetControllerAngles(trackedControllerRight.transform));
     
 	}
 
     // Return pitch value from -180 to 180 based on set up initial pitch
     Vector3 GetControllerAngles(Transform controllerRot)
     {
-		Vector3 rightXZ = Vector3.ProjectOnPlane(controllerRot.right, initUp).normalized;
+		Vector3 forwardXZ = Vector3.ProjectOnPlane(controllerRot.forward, initUp).normalized;
 		Vector3 upYX = Vector3.ProjectOnPlane(controllerRot.up, initForward).normalized;
 		Vector3 forwardZY = Vector3.ProjectOnPlane(controllerRot.forward, initRight).normalized;
 
 		float pitch = Mathf.Sign (controllerRot.forward.y) * Mathf.Acos (Vector3.Dot (forwardZY, initForward)) * Mathf.Rad2Deg;
-		float yaw = Mathf.Sign (controllerRot.right.z) * Mathf.Acos (Vector3.Dot (rightXZ, initRight)) * Mathf.Rad2Deg;
+		float yaw = Mathf.Sign (controllerRot.forward.x) * Mathf.Acos (Vector3.Dot (forwardXZ, initForward)) * Mathf.Rad2Deg;
 		float roll = Mathf.Sign (controllerRot.up.x) * Mathf.Acos (Vector3.Dot (upYX, initUp)) * Mathf.Rad2Deg;
 
 		return new Vector3(pitch , yaw, roll);
