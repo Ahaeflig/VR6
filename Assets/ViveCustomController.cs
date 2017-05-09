a792d4711523e682af58a2f133870c7e9801799c
Xavier Willemin <xavierwillemin@gmail.com>
2017-05-09 02:49:14 +0200

    Fix angles signs after calibration

--- Assets/ViveCustomController.cs
+++ Assets/ViveCustomController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -41,29 +41,29 @@ public class ViveCustomController : MonoBehaviour {
 	SteamVR_Controller.Device controllerLeft { get { return SteamVR_Controller.Input ((int)trackedControllerLeft.index); }}
 	SteamVR_Controller.Device controllerRight { get { return SteamVR_Controller.Input ((int)trackedControllerRight.index); }}
 
-    [SerializeField]
-    GameObject cubeTest;
+	[SerializeField]
+	GameObject cubeTest;
 
-    // Not const so that we can change these directly on the inspector
-    [SerializeField]
-    float PITCH_ANGLE_LOWER_THRESHOLD = 0f;
-    [SerializeField]
-    float PITCH_ANGLE_ZERO_THRESHOLD = 65f;    
-    [SerializeField]
-    float PITCH_ANGLE_UPPER_THRESHOLD = 130f;
+	// Not const so that we can change these directly on the inspector
+	[SerializeField]
+	float PITCH_ANGLE_LOWER_THRESHOLD = 0f;
+	[SerializeField]
+	float PITCH_ANGLE_ZERO_THRESHOLD = 65f;
+	[SerializeField]
+	float PITCH_ANGLE_UPPER_THRESHOLD = 130f;
 
-    private float lastForwardSpeed = 0f;
+	private float lastForwardSpeed = 0f;
 
-    //TODO make it change with button pressed
+	//TODO make it change with button pressed
 	Vector3 initRight = Vector3.right;
 	Vector3 initUp = Vector3.up;
 	Vector3 initForward = Vector3.forward;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-   
+
 	// Update is called once per frame
 	void Update () {
 
@@ -75,40 +75,41 @@ public class ViveCustomController : MonoBehaviour {
 		}
 
 		print(GetControllerAngles(trackedControllerRight.transform));
-    
+
 	}
 
-    // Return pitch value from -180 to 180 based on set up initial pitch
-    Vector3 GetControllerAngles(Transform controllerRot)
-    {
-		Vector3 forwardXZ = Vector3.ProjectOnPlane(controllerRot.forward, initUp).normalized;
-		Vector3 upYX = Vector3.ProjectOnPlane(controllerRot.up, initForward).normalized;
+	// Return pitch value from -180 to 180 based on set up initial pitch
+	Vector3 GetControllerAngles(Transform controllerRot)
+	{
 		Vector3 forwardZY = Vector3.ProjectOnPlane(controllerRot.forward, initRight).normalized;
+		float pitch = Mathf.Sign (Vector3.Dot(Vector3.Cross(forwardZY, initForward), initRight)) * Mathf.Acos (Vector3.Dot (forwardZY, initForward)) * Mathf.Rad2Deg;
 
-		float pitch = Mathf.Sign (controllerRot.forward.y) * Mathf.Acos (Vector3.Dot (forwardZY, initForward)) * Mathf.Rad2Deg;
-		float yaw = Mathf.Sign (controllerRot.forward.x) * Mathf.Acos (Vector3.Dot (forwardXZ, initForward)) * Mathf.Rad2Deg;
-		float roll = Mathf.Sign (controllerRot.up.x) * Mathf.Acos (Vector3.Dot (upYX, initUp)) * Mathf.Rad2Deg;
+		Vector3 forwardXZ = Vector3.ProjectOnPlane(controllerRot.forward, initUp).normalized;
+		float yaw = Mathf.Sign (Vector3.Dot(Vector3.Cross(forwardXZ, initForward), initUp)) * Mathf.Acos (Vector3.Dot (forwardXZ, initForward)) * Mathf.Rad2Deg;
+
+		Vector3 upYX = Vector3.ProjectOnPlane(controllerRot.up, initForward).normalized;
+		float roll = Mathf.Sign (Vector3.Dot(Vector3.Cross(upYX, initUp), initForward)) * Mathf.Acos (Vector3.Dot (upYX, initUp)) * Mathf.Rad2Deg;
 
 		return new Vector3(pitch , yaw, roll);
-    }
-		
-    //return val between [-1,1] use with rigidbody.velocity probably
-    // thresholds the pitch angle to return speed TODO fix XAVIER
-    float GetForwardSpeed(float pitch360)
-    {
-        //TODO Maybe we can change GetPitchZeroTo360 to return between [180 , -180] directly
-        if (pitch360 > 180)
-            pitch360 = pitch360 - 360;
-        
-        //Now use threshold to get a speed scale
-        if (pitch360 < PITCH_ANGLE_LOWER_THRESHOLD)
-            return lastForwardSpeed;
-        else if (pitch360 > PITCH_ANGLE_UPPER_THRESHOLD)
-            return lastForwardSpeed;
-        else {
-            lastForwardSpeed = (PITCH_ANGLE_ZERO_THRESHOLD - pitch360) * 2 / (PITCH_ANGLE_UPPER_THRESHOLD - PITCH_ANGLE_LOWER_THRESHOLD);
-            return lastForwardSpeed;
-        }
-    }
+	}
+
+	//return val between [-1,1] use with rigidbody.velocity probably
+	// thresholds the pitch angle to return speed TODO fix XAVIER
+	float GetForwardSpeed(float pitch360)
+	{
+		//TODO Maybe we can change GetPitchZeroTo360 to return between [180 , -180] directly
+		if (pitch360 > 180)
+		pitch360 = pitch360 - 360;
+
+		//Now use threshold to get a speed scale
+		if (pitch360 < PITCH_ANGLE_LOWER_THRESHOLD)
+		return lastForwardSpeed;
+		else if (pitch360 > PITCH_ANGLE_UPPER_THRESHOLD)
+		return lastForwardSpeed;
+		else {
+			lastForwardSpeed = (PITCH_ANGLE_ZERO_THRESHOLD - pitch360) * 2 / (PITCH_ANGLE_UPPER_THRESHOLD - PITCH_ANGLE_LOWER_THRESHOLD);
+			return lastForwardSpeed;
+		}
+	}
 
 }
