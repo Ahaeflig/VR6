c19b065bf198f20d797b199018ac9712d818fce5
Xavier Willemin <xavierwillemin@gmail.com>
2017-05-09 06:15:09 +0200

    Map controller angles to physical values

--- Assets/ViveCustomController.cs
+++ Assets/ViveCustomController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 //TODO Turn this class into  a Singleton
 public class ViveCustomController : MonoBehaviour {
 
@@ -44,17 +43,20 @@ public class ViveCustomController : MonoBehaviour {
 	[SerializeField]
 	GameObject cubeTest;
 
-	// Not const so that we can change these directly on the inspector
 	[SerializeField]
-	float PITCH_ANGLE_LOWER_THRESHOLD = 0f;
+	float PITCH_ANGLE_RANGE = 40f;
 	[SerializeField]
-	float PITCH_ANGLE_ZERO_THRESHOLD = 65f;
+	float YAW_ANGLE_RANGE = 110f;
 	[SerializeField]
-	float PITCH_ANGLE_UPPER_THRESHOLD = 130f;
+	float ROLL_ANGLE_RANGE = 150f;
 
-	private float lastForwardSpeed = 0f;
+	[SerializeField]
+	float PITCH_NEUTRAL_TOLERANCE = 4f;
+	[SerializeField]
+	float YAW_NEUTRAL_TOLERANCE = 8f;
+	[SerializeField]
+	float ROLL_NEUTRAL_TOLERANCE = 8f;
 
-	//TODO make it change with button pressed
 	Vector3 initRight = Vector3.right;
 	Vector3 initUp = Vector3.up;
 	Vector3 initForward = Vector3.forward;
@@ -74,7 +76,13 @@ public class ViveCustomController : MonoBehaviour {
 			initForward = trackedControllerRight.transform.forward;
 		}
 
-		print(GetControllerAngles(trackedControllerRight.transform));
+		Vector3 movement = ApplyControllerAnglesClamp(GetControllerAngles(trackedControllerRight.transform));
+
+		float forwardSpeed = GetForwardSpeed(movement.x);
+		float rightSpeed = GetRightSpeed(movement.y);
+		float rotationAngle = GetRotationAngle(movement.z);
+
+		print(new Vector3(forwardSpeed, rightSpeed, rotationAngle));
 
 	}
 
@@ -90,25 +98,42 @@ public class ViveCustomController : MonoBehaviour {
 		Vector3 upYX = Vector3.ProjectOnPlane(controllerRot.up, initForward).normalized;
 		float roll = Mathf.Sign (Vector3.Dot(Vector3.Cross(upYX, initUp), initForward)) * Mathf.Acos (Vector3.Dot (upYX, initUp)) * Mathf.Rad2Deg;
 
-		return new Vector3(pitch , yaw, roll);
+		return new Vector3(pitch, yaw, roll);
 	}
 
-	//return val between [-1,1] use with rigidbody.velocity probably
-	// thresholds the pitch angle to return speed TODO fix XAVIER
-	float GetForwardSpeed(float pitch360)
+	Vector3 ApplyControllerAnglesClamp(Vector3 unClampedAngles)
 	{
-		//TODO Maybe we can change GetPitchZeroTo360 to return between [180 , -180] directly
-		if (pitch360 > 180)
-		pitch360 = pitch360 - 360;
+		return new Vector3(
+			Mathf.Max(Mathf.Min(unClampedAngles.x, PITCH_ANGLE_RANGE/2), -PITCH_ANGLE_RANGE/2),
+			Mathf.Max(Mathf.Min(unClampedAngles.y, YAW_ANGLE_RANGE/YAW_ANGLE_RANGE), -YAW_ANGLE_RANGE/2),
+			Mathf.Max(Mathf.Min(unClampedAngles.z, ROLL_ANGLE_RANGE/2), -ROLL_ANGLE_RANGE/2)
+		);
+	}
 
-		//Now use threshold to get a speed scale
-		if (pitch360 < PITCH_ANGLE_LOWER_THRESHOLD)
-		return lastForwardSpeed;
-		else if (pitch360 > PITCH_ANGLE_UPPER_THRESHOLD)
-		return lastForwardSpeed;
-		else {
-			lastForwardSpeed = (PITCH_ANGLE_ZERO_THRESHOLD - pitch360) * 2 / (PITCH_ANGLE_UPPER_THRESHOLD - PITCH_ANGLE_LOWER_THRESHOLD);
-			return lastForwardSpeed;
+	float GetForwardSpeed(float pitchAngle)
+	{
+		if (Mathf.Abs(pitchAngle) >= PITCH_NEUTRAL_TOLERANCE) {
+			return pitchAngle / (PITCH_ANGLE_RANGE/2f);
+		} else {
+			return 0f;
+		}
+	}
+
+	float GetRightSpeed(float yawAngle)
+	{
+		if (Mathf.Abs(yawAngle) >= YAW_NEUTRAL_TOLERANCE) {
+			return yawAngle / (YAW_ANGLE_RANGE/2f);
+		} else {
+			return 0f;
+		}
+	}
+
+	float GetRotationAngle(float rollAngle)
+	{
+		if (Mathf.Abs(rollAngle) >= ROLL_NEUTRAL_TOLERANCE) {
+			return 180f * rollAngle / (ROLL_ANGLE_RANGE/2f);
+		} else {
+			return 0f;
 		}
 	}
 
