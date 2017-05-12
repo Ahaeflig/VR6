687bbd26c874f31def476008b1a6cd9014ce10b6
ahaeflig <adan.haefliger@gmail.com>
2017-05-12 11:46:14 +0200

    Basic game with keyboard control

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -7,34 +7,41 @@ public class Robot : MonoBehaviour {
     [SerializeField]
     GameObject controller;
 
-    private ViveCustomController controllerScript;
+    //private ViveCustomController controllerScript;
+    private KeyboardMovement controllerScript;
+
+    Rigidbody rigid; 
 
     [SerializeField]
-    float ForwardSpeedMultiplier = 100f;
+    float speedMultiplier = 5;
+    [SerializeField]
+    float rotationMultiplier = 1f;
+    [SerializeField]
+    float maxAngularVelocity = 1f;
 
-
-
-	// Use this for initialization
-	void Start () {
-
-        controllerScript = controller.GetComponent<ViveCustomController>();
-
-	}
+    // Use this for initialization
+    void Start () {
+        //TODO replace keyboard with vive when implemented
+        //controllerScript = controller.GetComponent<ViveCustomController>();
+        controllerScript = controller.GetComponent<KeyboardMovement>();
+        rigid = GetComponent<Rigidbody>();
+        rigid.maxAngularVelocity = maxAngularVelocity;
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
+        //Add Hover animation
+
+
 	}
 
+    // Called every physic tick, so more often than Update
     void FixedUpdate()
     {
-        //float ForwardSpeed = controllerScript.getControllerFrontSpeed();
-
-
-        //GetComponent<Rigidbody>().velocity = this.transform.forward * ForwardSpeed * ForwardSpeedMultiplier;
-
+        //For movement along XZ plane without inertia
+        //rigid.velocity = this.transform.forward * speed.x * speedMultiplier + this.transform.right * speed.y * speedMultiplier;
+        Vector3 speed = controllerScript.getSpeedVector();
+        rigid.AddRelativeForce(new Vector3(speed.y * speedMultiplier, 0f, speed.x * speedMultiplier));
+        rigid.AddRelativeTorque(new Vector3(0, speed.z * rotationMultiplier, 0), ForceMode.Acceleration);
     }
-
-
 
 }
---
+++ Assets/Turret.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Turret : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject Projectile;
+    [SerializeField]
+    public float FireDelay = 5f;
+
+
+    private DigitalRuby.PyroParticles.FireBaseScript currentPrefabScript;
+    //FirePosition child must be at index 0
+    private Transform firePositionTransform;
+    private IEnumerator shootCoroutine;
+
+    // Use this for initialization
+    void Start () {
+        currentPrefabScript = Projectile.GetComponent<DigitalRuby.PyroParticles.FireBaseScript>();
+        firePositionTransform = transform.GetChild(0).GetComponent<Transform>();
+
+        shootCoroutine = WaitAndFire(FireDelay);
+        StartCoroutine(shootCoroutine);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    // every 2 seconds perform the print()
+    private IEnumerator WaitAndFire(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            GameObject currentPrefabObject = GameObject.Instantiate(Projectile, firePositionTransform);
+            //currentPrefabScript = currentPrefabObject.GetComponent<DigitalRuby.PyroParticles.FireBaseScript>();
+
+
+        }
+    }
+
+}
