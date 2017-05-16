0e9fb126c78f0ddd0402f74447f40215a78c049c
Jonathan Link <jonathanlink.email@gmail.com>
2017-05-16 11:32:06 +0200

    Update with IP of EPFL

--- Assets/GameNetwork/NetworkInfo.cs
+++ Assets/GameNetwork/NetworkInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NetworkInfo {
-	public static string serverIp = "192.168.1.111"; // "localhost"
+	public static string serverIp = "128.179.140.25"; // "localhost", 192.168.1.111
 	public static int serverPort = 4444;
 };
 
